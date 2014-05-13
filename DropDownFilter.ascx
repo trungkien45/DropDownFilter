811d6a72d6e405cf0bd1649ea77d62e99f2cb2b9
trungkien45 <ntkien45-diendan@yahoo.com.vn>
2014-05-13 14:42:16 +0700

    First
    First

---
+++ DropDownFilter.ascx
@@ -0,0 +1,42 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="DropDownFilter.ascx.cs" Inherits="WebApplication5.Control.Common.DropDownFilter" %>
+<script src="../../Scripts/jquery-1.4.1.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(function () {
+        var $txt = $('#<%=TextBox1.ClientID %>');
+        var $ddl = $('#<%=DropDownList1.ClientID%>');
+        var $items = $('#<%=DropDownList1.ClientID %> option');
+        $txt.keyup(function () {
+            <%=this.ClientID %>searchDDL($txt.val()); 
+        });
+        $ddl.change(function(){
+            var criteria_id =  $(':selected',this).text();
+            $txt.val(criteria_id);
+        });
+        function <%=this.ClientID%>searchDDL(item){
+            $ddl.empty();
+            var exp= new RegExp(item,"i");
+            var arr= $.grep($items,function(n){
+                return exp.test($(n).text());
+            });
+            if (arr.length > 0) {
+                //countItemsFound(arr.length);
+                $.each(arr, function() {
+                    $ddl.append(this);
+                    $ddl.get(0).selectedIndex = 0;
+                });
+            }
+            else {
+                //countItemsFound(arr.length);
+                $ddl.append("<option>No Items Found</option>");
+            }
+        }
+    });
+</script>
+<div style="float:left">
+<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ForeColor="Red" ControlToValidate="DropDownList1"
+    ErrorMessage="">*</asp:RequiredFieldValidator>
+<br />
+<asp:DropDownList ID="DropDownList1" runat="server" 
+   Width="125px" ondatabound="DropDownList1_DataBound">
+</asp:DropDownList>
+</div>
---
+++ DropDownFilter.ascx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+
+namespace WebApplication5.Control.Common
+{
+    public partial class DropDownFilter : System.Web.UI.UserControl
+    {
+        [BindableAttribute(true)]
+        [ThemeableAttribute(false)]
+        public object DataSource {
+            get {
+                return DropDownList1.DataSource;
+            }
+            set { DropDownList1.DataSource = value; }
+        }
+
+        public bool NeedVal {
+            get { return RequiredFieldValidator1.Enabled; }
+            set { RequiredFieldValidator1.Enabled = value; }
+        }
+        public string DataSourceID { get { return DropDownList1.DataSourceID; }
+            set { DropDownList1.DataSourceID = value; }
+        }
+        public string DataTextField {
+            get { return DropDownList1.DataTextField; }
+            set { DropDownList1.DataTextField = value; }
+        }
+        public string DataValueField {
+            get { return DropDownList1.DataValueField; }
+            set { DropDownList1.DataValueField = value; }
+        }
+        public ListItem SelectedItem
+        {
+            get
+            {
+                return DropDownList1.SelectedItem;
+            }
+        }
+        public override void DataBind()
+        {
+            base.DataBind();
+            
+            DropDownList1.DataBind();
+        }
+        public ListItemCollection Items
+        {
+            get { return DropDownList1.Items; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RequiredFieldValidator1.ValidationGroup = this.ClientID;
+            //DropDownList1.DataTextField
+            //DropDownList1.s
+            //DropDownList1.DataSource
+        }
+
+        protected void DropDownList1_DataBound(object sender, EventArgs e)
+        {
+            DropDownList1.Items.Insert(0, "");
+        }
+    }
+}
